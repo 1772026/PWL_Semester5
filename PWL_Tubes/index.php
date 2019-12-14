ae9b0d6d492e562fa27af6df0162f00f959daea9
joshuasuhe <54823822+joshuasuhe@users.noreply.github.com>
2019-12-14 10:42:49 +0700

    init UI

---
+++ PWL_Tubes/index.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Tubes PWL</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,900|Oswald:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+<div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+            <div class="site-mobile-menu-close mt-3">
+                <span class="icon-close2 js-menu-toggle"></span>
+            </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+    </div>
+
+
+    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+
+        <div class="container">
+            <div class="row align-items-center">
+
+                <div class="col-6 col-xl-2">
+                    <h1 class="mb-0 site-logo m-0 p-0"><a href="index.php" class="mb-0">TUBES PWL</a></h1>
+                </div>
+
+                <div class="col-12 col-md-10 d-none d-xl-block">
+                    <nav class="site-navigation position-relative text-right" role="navigation">
+
+                        <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                            <li><a href="" class="nav-link">Link</a></li>
+                            <li><a href="" class="nav-link">Link</a></li>
+                            <li><a href="" class="nav-link">Link</a></li>
+                            <li><a href="" class="nav-link">Link</a></li>
+                            <li><a href="" class="nav-link">Link</a></li>
+                            <li><a href="" class="nav-link">Link</a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+
+                <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3"><a href="#"
+                                                                            class="site-menu-toggle js-menu-toggle text-white float-right"><span
+                                class="icon-menu h3"></span></a></div>
+
+            </div>
+        </div>
+
+    </header>
+
+
+    <div class="site-block-wrap">
+        <div class="owl-carousel with-dots">
+            <div class="site-blocks-cover overlay overlay-2" style="background-image: url(images/ice-by-cgr.jpg);"
+                 data-aos="fade" id="home-section">
+
+
+                <div class="container">
+                    <div class="row align-items-center justify-content-center">
+                        <div class="col-md-6 mt-lg-5 text-center">
+                            <h1 class="text-shadow">Bla Bla Bla</h1>
+                            <p class="mb-5 text-shadow">BLA BLA BLA BLA</p>
+                            <p><a href="#" target="_blank" class="btn btn-primary px-5 py-3">Get Started</a></p>
+
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+            <div class="site-blocks-cover overlay overlay-2"
+                 style="background-image: url(images/PNEK2UTRMQI6PDAXKM6FFMXQCQ.jpg);" data-aos="fade"
+                 id="home-section">
+
+
+                <div class="container">
+                    <div class="row align-items-center justify-content-center">
+                        <div class="col-md-6 mt-lg-5 text-center">
+                            <h1 class="text-shadow">Bla Bla Bla</h1>
+                            <p class="mb-5 text-shadow">BLA BLA BLA BLA</p>
+                            <p><a href="#" target="_blank" class="btn btn-primary px-5 py-3">Get Started</a></p>
+
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+    <div class="site-section" id="properties-section">
+        <div class="container">
+            <div class="row large-gutters">
+                <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+                    <div class="ftco-media-1">
+                        <div class="ftco-media-1-inner">
+                            <a href="" class="d-inline-block mb-4"><img src="images/tomcruise.jpg"
+                                                                        alt="FImageo"
+                                                                        class="img-fluid"></a>
+                            <div class="ftco-media-details">
+                                <h3>FILM.</h3>
+                                <p>Action, Horror</p>
+                                <strong></strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+                    <div class="ftco-media-1">
+                        <div class="ftco-media-1-inner">
+                            <a href="" class="d-inline-block mb-4"><img src="images/tomcruise.jpg"
+                                                                        alt="FImageo"
+                                                                        class="img-fluid"></a>
+                            <div class="ftco-media-details">
+                                <h3>FILM.</h3>
+                                <p>Action, Horror</p>
+                                <strong></strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+                    <div class="ftco-media-1">
+                        <div class="ftco-media-1-inner">
+                            <a href="" class="d-inline-block mb-4"><img src="images/tomcruise.jpg"
+                                                                        alt="FImageo"
+                                                                        class="img-fluid"></a>
+                            <div class="ftco-media-details">
+                                <h3>FILM.</h3>
+                                <p>Action, Horror</p>
+                                <strong></strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row" align="right">
+                <div class="col-md-8">
+                    <div class="row">
+                        <div class="col-md-5">
+                            <h2 class="footer-heading mb-4">About Us</h2>
+                            <p>This is about us</p>
+                        </div>
+                        <div class="col-md-3 mx-auto">
+                            <h2 class="footer-heading mb-4">Quick Links</h2>
+                            <ul class="list-unstyled">
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="mb-4">
+                        <div class="">
+                            <h2 class="footer-heading mb-4">Follow Us</h2>
+                            <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+                            <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+                            <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+            <div class="col-md-12">
+                <div class="border-top pt-5">
+                    <p class="copyright">
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script>
+                        All rights reserved
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </p>
+                </div>
+            </div>
+
+        </div>
+</div>
+</footer>
+
+</div> <!-- .site-wrap -->
+
+<a href="#top" class="gototop"><span class="icon-angle-double-up"></span></a>
+
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.countdown.min.js"></script>
+<script src="js/bootstrap-datepicker.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/jquery.fancybox.min.js"></script>
+<script src="js/jquery.sticky.js"></script>
+
+
+<script src="js/main.js"></script>
+
+</body>
+</html>
