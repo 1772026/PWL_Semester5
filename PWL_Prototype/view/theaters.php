122dd46d8b55340c668f69cb4e216aa7c90f6461
joshuasuhe <54823822+joshuasuhe@users.noreply.github.com>
2020-01-05 08:45:22 +0700

    update new

---
+++ PWL_Prototype/view/theaters.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Tubes PWL</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/form.css">
+    <link rel="stylesheet" href="css/seat.css">
+</head>
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+<div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+            <div class="site-mobile-menu-close mt-3">
+                <span class="icon-close2 js-menu-toggle"></span>
+            </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+    </div>
+
+
+    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner"
+            style="background-color: rgb(20,20,20);">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-6 col-xl-2 text-center">
+                    <h1 class="mb-0 site-logo m-0 p-0"><a href="index.php" class="mb-0">CINEMONCE</a></h1>
+                </div>
+
+                <div class="col-12 col-md-10 d-none d-xl-block">
+                    <nav class="site-navigation position-relative text-right" role="navigation">
+
+                        <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                            <li><a href="movies.php" class="nav-link">Movies</a></li>
+                            <li><a href="theaters.php" class="nav-link">Theaters</a></li>
+                            <li><a class="nav-link" href="#" data-target="#login" data-toggle="modal">Sign In</a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3"><a href="#"
+                                                                            class="site-menu-toggle js-menu-toggle text-white float-right"><span
+                                class="icon-menu h3"></span></a></div>
+            </div>
+        </div>
+    </header>
+
+    <div id="login" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body bg-white">
+                    <button data-dismiss="modal" class="close text-black">&times;</button>
+                    <h4 style="color: black">Login</h4>
+                    <form>
+                        <input type="text" name="username" class="username form-control"
+                               placeholder="Username" style="border-color: lightgrey; opacity: 1"/>
+                        <input type="password" name="password" class="password form-control"
+                               placeholder="Password" style="border-color: lightgrey; opacity: 1"/>
+                        <h6 style=""><a href=""> Forgot Password?</a></h6>
+                        <h6 style="color: black"> Not a member? <a href="">Sign Up.</a></h6>
+                        <input class="btn login btn-primary" type="submit" value="Login"/>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div style="background-color: black;top: 0;height: 90px"></div>
+
+    <div class="site-section pr-5" id="properties-section" align="center">
+        <div class="container " style="border: 1px">
+            <div class="dropdown justify-content-center">
+                <button class="btn btn-primary dropdown-toggle bg-white text-black"
+                        style="width: 75%; border-color: black;" type="button" data-toggle="dropdown">Dropdown Example
+                    <span class="caret"></span></button>
+                <ul class="dropdown-menu" style="width: 75%">
+                    <li><a href="#" style="color: black">HTML</a></li>
+                    <li><a href="#" style="color: black">CSS</a></li>
+                    <li><a href="#" style="color: black">JavaScript</a></li>
+                </ul>
+            </div>
+            <div class="dropdown justify-content-center">
+                <button class="btn btn-primary dropdown-toggle bg-white text-black"
+                        style="width: 75%; border-color: black;" type="button" data-toggle="dropdown">Dropdown Example
+                    <span class="caret"></span></button>
+                <ul class="dropdown-menu" style="width: 75%">
+                    <li><a href="#" style="color: black">HTML</a></li>
+                    <li><a href="#" style="color: black">CSS</a></li>
+                    <li><a href="#" style="color: black">JavaScript</a></li>
+                </ul>
+            </div>
+            <div class="dropdown justify-content-center">
+                <button class="btn btn-primary dropdown-toggle bg-white text-black"
+                        style="width: 75%; border-color: black;" type="button" data-toggle="dropdown">Dropdown Example
+                    <span class="caret"></span></button>
+                <ul class="dropdown-menu" style="width: 75%">
+                    <li><a href="#" style="color: black">HTML</a></li>
+                    <li><a href="#" style="color: black">CSS</a></li>
+                    <li><a href="#" style="color: black">JavaScript</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <footer class="site-footer" style="background-color: rgb(20,20,20); bottom: 0; width: 100%;">
+        <div class="container" align="center">
+            <div class="row" align="center" style="height: 7vw">
+                <div class="col-md-8">
+                    <div class="row">
+                        <div class="col-md-5">
+                            <h2 class="footer-heading mb-4">About Us</h2>
+                            <p>This is about us</p>
+                        </div>
+                        <div class="col-md-3 mx-auto">
+                            <h2 class="footer-heading mb-4">Links</h2>
+                            <ul class="list-unstyled">
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="mb-4">
+                        <div class="">
+                            <h2 class="footer-heading mb-4">Follow Us</h2>
+                            <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+                            <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+                            <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row pt-5 mt-5 mr-5 text-center" align="center" style="height: 6vw; align-items: center;">
+            <div class="col-md-12 text-center">
+                <div class="border-top pt-5 p-4">
+                    <p class="copyright">
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script>
+                        All rights reserved
+                    </p>
+                </div>
+            </div>
+        </div>
+</div>
+</footer>
+
+</div> <!-- .site-wrap -->
+
+<a href="#top" class="gototop"><span class="icon-angle-double-up"></span></a>
+
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.countdown.min.js"></script>
+<script src="js/bootstrap-datepicker.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/jquery.fancybox.min.js"></script>
+<script src="js/jquery.sticky.js"></script>
+
+<script src="js/main.js"></script>
+
+</body>
+</html>
